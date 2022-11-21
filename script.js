efa36300be1abf10c214b653d00417ad3fef0c31
CALVINCE <calvince.ogwel@gmail.com>
2022-11-21 18:50:32 +0300

    script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+let students = {
+    name: {
+        student1: "Brian",
+        student2: "Happiness"
+    }
+    class: {
+        class1 = "javascript",
+        class2 = "ruby"
+    }
+}
+console.log(`this student name is ${student.name.student1} and he is in the ${student.class.class2} class` )
